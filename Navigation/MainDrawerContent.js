007457e7fbee488c65d9134dfa8717486d9493c7
Dominique Kanyik <domykanyiktesh01@gmail.com>
2021-02-12 16:26:27 +0200

    Riders App 2.1.281
    [*] Version strings update.

--- Navigation/MainDrawerContent.js
+++ Navigation/MainDrawerContent.js
@@ -88,7 +88,7 @@ export function MainDrawerContent(props) {
               color: '#AFAFAF',
               textAlign: 'right',
             }}>
-            v2.1.280
+            v2.1.281
           </Text>
         </View>
       </SafeAreaView>
