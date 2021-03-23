f8fd03e14c726081735c6a7866997f5faeaac1b9
Dominique Kanyik <domykanyiktesh01@gmail.com>
2021-03-23 12:15:58 +0200

    Riders App 2.1.345
    [*] Crashlytics integrated for Android.

--- Navigation/MainDrawerContent.js
+++ Navigation/MainDrawerContent.js
@@ -92,7 +92,7 @@ export function MainDrawerContent(props) {
               color: '#AFAFAF',
               textAlign: 'right',
             }}>
-            v2.1.343
+            v2.1.344
           </Text>
         </View>
       </SafeAreaView>
