e683f9fd173d44ac814c13572afc5b9b3255aa04
Dominique Kanyik <domykanyiktesh01@gmail.com>
2021-04-18 19:46:33 +0200

    Riders App 2.1.368
    [*] Slight modifications.

--- Navigation/MainDrawerContent.js
+++ Navigation/MainDrawerContent.js
@@ -92,7 +92,7 @@ export function MainDrawerContent(props) {
               color: '#AFAFAF',
               textAlign: 'right',
             }}>
-            v2.1.366
+            v2.1.368
           </Text>
         </View>
       </SafeAreaView>
