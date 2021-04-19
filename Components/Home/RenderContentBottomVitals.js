359b2c447f052e4276c438434db03559dba87a7c
Dominique Kanyik <domykanyiktesh01@gmail.com>
2021-04-19 03:32:49 +0200

    Riders App 2.1.368
    [*] Remove SMS permissions from manifest.

--- Components/Home/RenderContentBottomVitals.js
+++ Components/Home/RenderContentBottomVitals.js
@@ -2433,7 +2433,7 @@ class RenderContentBottomVitals extends React.PureComponent {
                               : 'Uber Move Text Medium',
                         },
                       ]}>
-                      N$45
+                      N$48
                     </Text>
                   </Text>
                 </View>
